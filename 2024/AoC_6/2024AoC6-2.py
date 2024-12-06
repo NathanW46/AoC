330c92d2eb6f872b5a99fa085139a7a497db96fe
Nathan Whittington <83149241+NathanW46@users.noreply.github.com>
2024-12-06 17:22:41 -0500

    Update 2024AoC6-2.py

--- 2024/AoC_6/2024AoC6-2.py
+++ 2024/AoC_6/2024AoC6-2.py
@@ -3,6 +3,9 @@ from datetime import datetime
 import numpy as np
 import re
 
+#not happening
+
+
 year = 2024
 day = 6
 filepath = os.path.join(os.getcwd(), f'{year}/AoC_{day}/2024AoC{day}_sample.txt')

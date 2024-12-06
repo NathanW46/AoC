2b68cde47bb82d5e3803291873468bcbcc4d3662
Nathan Whittington <24ndwhittington@gmail.com>
2024-12-06 14:42:10 -0500

    test

--- 2024/AoC_6/2024AoC6-1.py
+++ 2024/AoC_6/2024AoC6-1.py
@@ -7,3 +7,6 @@ year = 2024
 day = 6
 filepath = os.path.join(os.getcwd(), f'{year}/AoC_{day}/2024AoC{day}_sample.txt')
 # filepath = os.path.join(os.getcwd(), f'{year}/AoC_{day}/2024AoC{day}_input.txt')
+
+
+testasdjksadadasdada

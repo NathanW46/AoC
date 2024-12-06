2912ebd7ded9f807752fc61ce35d74a42cbc24dc
Nathan Whittington <24ndwhittington@gmail.com>
2024-12-06 14:41:12 -0500

    Test

--- 2024/AoC_6/2024AoC6-1.py
+++ 2024/AoC_6/2024AoC6-1.py
@@ -5,5 +5,5 @@ import re
 
 year = 2024
 day = 6
-# filepath = os.path.join(os.getcwd(), f'{year}/AoC_{day}/2024AoC{day}_sample.txt')
-filepath = os.path.join(os.getcwd(), f'{year}/AoC_{day}/2024AoC{day}_input.txt')
+filepath = os.path.join(os.getcwd(), f'{year}/AoC_{day}/2024AoC{day}_sample.txt')
+# filepath = os.path.join(os.getcwd(), f'{year}/AoC_{day}/2024AoC{day}_input.txt')

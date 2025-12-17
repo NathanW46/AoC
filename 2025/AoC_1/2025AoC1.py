030eca1d5f2a3af5c341c6cca956c079083fc36f
Nathan Whittington <24ndwhittington@gmail.com>
2025-12-17 16:08:14 -0500

    2025-1 cleanup

--- 2025/AoC_1/2025AoC1.py
+++ 2025/AoC_1/2025AoC1.py
@@ -1,9 +1,34 @@
-import os
-from datetime import datetime
 import numpy as np
-import re
 
-year = 2025
-day = 1
-filepath = os.path.join(os.getcwd(), f'{year}/AoC_{day}/2024AoC{day}_sample.txt')
-# filepath = os.path.join(os.getcwd(), f'{year}/AoC_{day}/2024AoC{day}_input.txt')
+filename = "2025AoC1_input.txt"
+# filename='2025AoC1_sample.txt'
+
+
+dial = 50
+pswd = 0
+pt2 = 0
+
+with open(filename, "r") as file:
+    for i in file:
+        # if True:
+        #     for i in test:
+        old = dial
+        # part 2
+
+        pt2 += int(i[1:]) // 100
+        n = int(i[1:]) % 100
+        if i[0] == "L":
+            dial -= int(n)
+        elif i[0] == "R":
+            dial += int(n)
+
+        new = dial
+
+        # part 1
+        if not (0 < dial < 100) and old != 0:
+            pt2 += 1
+        dial %= 100
+        if dial == 0:
+            pswd += 1
+
+print("part 1:", pswd, "\npart 2:", pt2)
